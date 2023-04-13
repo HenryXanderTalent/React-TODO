d45163e1706cae2034f545acbaa81e83e9c28191
HenryXanderTalent <henry.opara@xandertalent.com>
2023-04-13 19:42:47 +0100

    added checkboxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import "./styles.css"
 
 export default function App() {
-  return <form className="new-item-form">
+  return (
+    <>
+    <form className="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New Item</label>
         <input type="text" id="item" />
@@ -9,6 +11,26 @@ export default function App() {
 
       <button className="btn">Add</button>
     </form>
+    <h1 className="header">Todo List</h1>
+
+    <ul className="list">
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 2
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+    </ul>
+    </>
+  )
 }
 
 
