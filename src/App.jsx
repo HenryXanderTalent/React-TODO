70ddd7cf2a532dc4233eb6700108e57547eb365d
HenryXanderTalent <henry.opara@xandertalent.com>
2023-04-14 06:17:49 +0100

    added check button feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { NewTodoForm } from "./NewTodoForm"
 import "./styles.css"
+import { TodoList } from "./TodoList"
 
 export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
 
-  const [todos, setTodos] = useState([])
+    return JSON.parse(localValue)
+  })
 
-  function addTodo(title){
-        setTodos((currentTodos) => {
-          return [
-            ...currentTodos, 
-            {id: crypto.randomUUID(), title, completed: false },
-          ]
-        })
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
   }
 
   function toggleTodo(id, completed) {
@@ -35,64 +44,9 @@ export default function App() {
 
   return (
     <>
-    <NewTodoForm />
-    <h1 className="header">Todo List</h1>
-
-    <ul className="list">
-      {todos.length === 0 && "No Todos"}
-      {todos.map(todo => {
-        return (
-          <li key={todo.id}>
-            <label>
-              <input type="checkbox" checked={todo.completed} onchange={e => toggleTodo(todo.id, e.target.checked)}/>
-              {todo.title}
-            </label>
-            <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
-          </li>
-          )
-      })}
-
-    </ul>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
   )
 }
-
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <div className="App">
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://reactjs.org" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -1,34 +1,29 @@
 import { useState } from "react"
 
-export function NewTodoForm(props) {
-    props.onSubmit
+export function NewTodoForm({ onSubmit }) {
+  const [newItem, setNewItem] = useState("")
 
-    const [newItem, setNewItem] = useState("")
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (newItem === "") return
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        if (newItem === "") return
-    
-        props.onSubmit(newItem)
-    
-        setNewItem("")
-    }
-    
+    onSubmit(newItem)
 
-    return (
-        
-        <form onSubmit={handleSubmit} className="new-item-form">
-          <div className="form-row">
-            <label htmlFor="item">New Item</label>
-            <input 
-              value={newItem} 
-              onChange={e => setNewItem(e.target.value)}
-              type="text" 
-              id="item" 
-            />
-          </div>
-    
-          <button className="btn">Add</button>
-        </form>
-    )    
+    setNewItem("")
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input
+          value={newItem}
+          onChange={e => setNewItem(e.target.value)}
+          type="text"
+          id="item"
+        />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+  )
 }
